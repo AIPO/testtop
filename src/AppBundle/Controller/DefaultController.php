d405ddabb81663e99ac23816b0deb3f20d7e67f4
Aipo <p.vaitonis@gmail.com>
2017-08-19 12:06:16 +0300

    Added bootstrap CDN and styled list table

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,15 +15,17 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
+
     /**
      * @Route ("/list")
      */
     public function listAction()
     {
         $json = json_decode(file_get_contents('https://gist.githubusercontent.com/emodus/27d245484a85c2286722b9d146c53354/raw/c9af224580a22cbde969127527c4500e3f7d2a9e/dummyFeed '), true);
-        return $this->render('default/list.html.twig', array('items'=>$json.items) );
+        $items = $json['items'];
+        return $this->render('default/list.html.twig', array('items' => $items));
     }
 }
